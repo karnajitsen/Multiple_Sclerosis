59ba9e287f8a62d934cc939cf2985512e534b737
Karnajit Sen <SenKa@basfad.basf.net>
2018-07-01 21:33:41 +0200

    Data prepared

--- src/MultipleScerosis.py
+++ src/MultipleScerosis.py
@@ -24,21 +24,10 @@ def main():
     dfUSTreated.drop(columns = ['testResultMetricId', 'testResultMetricCreatedOn'], inplace =True)
     dfUSControl.drop_duplicates(inplace = True)
     dfUSTreated.drop_duplicates(inplace = True)
-    dfUSControlTest = pd.DataFrame(data = dfUSControl.floodlightOpenId.unique(), columns = {"floodlightOpenId"})
-    dfUSTreatedTest =  pd.DataFrame(data = dfUSTreated.floodlightOpenId.unique(), columns = {"floodlightOpenId"})
-    d = pd.DataFrame()
-    d1 = pd.DataFrame()
-    for t in tmn:
-        d = pd.DataFrame()
-        d1 = pd.DataFrame()
-        d[[ "floodlightOpenId" , t]] = dfUSTreated[dfUSTreated.testMetricName == t][["floodlightOpenId", "testResultMetricValue"]]
-        #d.dropna(inplace = True)
-        dfUSTreatedTest = pd.merge(dfUSTreatedTest, d, how = "left", on = "floodlightOpenId")
-        #d1[[ "floodlightOpenId" , t]]= dfUSControl[dfUSControl.testMetricName == t][["floodlightOpenId", "testResultMetricValue"]]
-        #dfUSControlTest = pd.concat([dfUSControlTest, d1], axis = 1)
-    
-    
-    
+    dfUSControlTest = pd.DataFrame(pd.pivot_table(dfUSControl, index = "floodlightOpenId", columns = "testMetricName", values = "testResultMetricValue").to_records())
+    dfUSTreatedTest = pd.DataFrame(pd.pivot_table(dfUSTreated, index = "floodlightOpenId", columns = "testMetricName", values = "testResultMetricValue").to_records())
+    dfUSControlTest.fillna(0, inplace = True)
+    dfUSTreatedTest.fillna(0, inplace = True)
 
 if __name__ == '__main__':
     main()
