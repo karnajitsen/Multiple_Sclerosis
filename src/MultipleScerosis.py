0a4758ddaaf8ef736a9ebb87ee71ee11b3814998
Karnajit Sen <SenKa@basfad.basf.net>
2018-08-03 00:32:01 +0200

    some model fitted

--- src/MultipleScerosis.py
+++ src/MultipleScerosis.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats as st
 pd.set_option("display.max_rows", 1000)
 pd.set_option("display.max_columns", 1000)
-
+from sklearn import linear_model
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
 
 def readData(fpath):
@@ -21,12 +24,43 @@ def main():
     dfUSTreated = dfUS[dfUS.participantIsControl == False].copy()
     dfUSControl.drop(columns = ['testResultMetricId', 'testResultMetricCreatedOn'], inplace = True)
     dfUSTreated.drop(columns = ['testResultMetricId', 'testResultMetricCreatedOn'], inplace =True)
-    dfUSControl.drop_duplicates(inplace = True)
-    dfUSTreated.drop_duplicates(inplace = True)
-    dfUSControlTest = pd.DataFrame(pd.pivot_table(dfUSControl, index = "floodlightOpenId", columns = "testMetricName", values = "testResultMetricValue").to_records())
-    dfUSTreatedTest = pd.DataFrame(pd.pivot_table(dfUSTreated, index = "floodlightOpenId", columns = "testMetricName", values = "testResultMetricValue").to_records())
+    #dfUSControl.drop_duplicates(inplace = True)
+    #dfUSTreated.drop_duplicates(inplace = True)
+    dfUSControlTest = pd.DataFrame(pd.pivot_table(dfUSControl, index = "floodlightOpenId", 
+                                                  columns = "testMetricName", 
+                                                  values = "testResultMetricValue",
+                                                  aggfunc = 'max').to_records())
+    dfUSTreatedTest = pd.DataFrame(pd.pivot_table(dfUSTreated, index = "floodlightOpenId", 
+                                                  columns = "testMetricName", 
+                                                  values = "testResultMetricValue",
+                                                  aggfunc = 'max').to_records())
+    
+    dfUSControlTest = pd.DataFrame(pd.pivot_table(dfUSControl, index = "floodlightOpenId", 
+                                                  columns = "testMetricName", 
+                                                  values = "testResultMetricValue",
+                                                  ).to_records())
+    dfUSTreatedTest = pd.DataFrame(pd.pivot_table(dfUSTreated, index = "floodlightOpenId", 
+                                                  columns = "testMetricName", 
+                                                  values = "testResultMetricValue",
+                                                  ).to_records())
     dfUSControlTest.fillna(0, inplace = True)
     dfUSTreatedTest.fillna(0, inplace = True)
+    dfUSControlTest.insert(0,"id", range(0, len(dfUSControlTest)))
+    dfUSTreatedTest.insert(0,"id", range(0, len(dfUSTreatedTest)))
+    dfUSTreatedTest["class"] = 1
+    dfUSControlTest["class"] = 2
+    fitmodel(dfUSTreatedTest, dfUSControlTest)
+
+def fitmodel(c1, c2):
+    clf = LinearDiscriminantAnalysis()
+    
+def plotFeatures(df1, df2, xaxis = "", featurenames = [""]):
+    
+    for feature in featurenames:
+        #plt.figure()
+        ax = df1.plot(x = xaxis, y = feature, color = 'b', kind = 'scatter', title = feature)
+        df2.plot(x = xaxis, y = feature, color = 'r', kind = 'scatter', ax = ax)
+    
 
 if __name__ == '__main__':
     main()
